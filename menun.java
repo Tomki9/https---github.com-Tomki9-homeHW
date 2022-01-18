fea040c08cab7433aaa1f7234598c1ba4801bd86
Tomki9 <88100719+Tomki9@users.noreply.github.com>
2022-01-18 16:02:55 -0500

    first commit

---
+++ menun.java
@@ -0,0 +1,26 @@
+public class Main {
+    
+}
+
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+  /* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
+  
